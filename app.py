800cbbc278f9789245e9e8809b320e3376c3a9dd
GvanGReaper <georgegaming.gr@gmail.com>
2023-06-30 21:53:53 +0300

    comet hehe

--- app.py
+++ app.py
@@ -1,13 +1,21 @@
-from flask import Flask
+from flask import Flask, request, render_template
+from decode import *
 
 app = Flask(__name__)
 
 
 
-@app.route('/')
-def index():
-    return 'Index Page'
+from flask import render_template
 
-@app.route('/hello')
-def hello():
-    return 'Hello, World'
+@app.route('/')
+def main(name=None):
+   return render_template('index.html', name=name)
+
+@app.route('/success',methods = ['POST'])
+def success(): 
+   if request.method == 'POST':  
+      f = request.files['file']
+      decode_file_content(f)
+      return render_template("success.html", name = f.filename)  
+
+
---
+++ decode.py
@@ -0,0 +1,5 @@
+
+
+def decode_file_content(file):
+   for line in file:
+      print(line)
