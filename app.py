cd8f1d8578292c4ffb33fb3c1b2729df2eba56cc
GvanGReaper <georgegaming.gr@gmail.com>
2023-07-01 20:40:59 +0300

    ready for you noudi

--- app.py
+++ app.py
@@ -4,6 +4,7 @@ from decode import *
 app = Flask(__name__)
 
 
+
 @app.route("/",methods=['POST','GET'])
 def main():
    return render_template("index.html")
@@ -12,9 +13,13 @@ def main():
 @app.route("/pleaseWait",methods = ['POST','GET']) 
 def answers_submitted():
    if request.method == 'POST':
-      data = request.form
-            
-
+      file = request.files.get("fileName")
+      file_content = file.read()   
+      #SENT TO ENCODE
+      char_list = []
+      for ch in file_content:
+         char_list.append(chr(ch))
+         
    return render_template("success.html")
 
 
